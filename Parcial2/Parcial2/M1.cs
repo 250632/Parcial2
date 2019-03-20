a15077b2bc61a6f27db27905ff757afc0d0c6527
250632 <48265461+250632@users.noreply.github.com>
2019-03-19 19:34:15 -0600

    Add files via upload

---
+++ Parcial2/Parcial2/M1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial2
+{
+    class M1
+    {
+       
+            public void Multi()
+        {
+            int n1,n2,pro;
+            Console.WriteLine("Ingrese un numero Entero");
+            n1 = int.Parse(Console.ReadLine());
+            if(n1==0)
+            {
+                Console.WriteLine("La multiplicacion es 0");
+            }
+            else if(n1>0&n1<0)
+            {
+                Console.WriteLine("Ingrese un numero Entero");
+                n2 = int.Parse(Console.ReadLine());
+                pro = n1 * n2;
+                Console.WriteLine("El Producto de los numeros es :"+pro);
+            }
+        }
+
+      
+        public void Method2()
+        {
+            int n3, n4, div;
+            Console.WriteLine("Ingrese un numero Entero");
+            n3 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese un numero Entero");
+            n4 = int.Parse(Console.ReadLine());
+
+            if (n4>0&n4<0)
+            {
+                div = n3 / n4;
+                Console.WriteLine("La division de los numeros es :"+div);
+            }
+
+            else if(n4==0)
+            {
+                Console.WriteLine("Error: No se puede dividir entre cero");
+            }
+        }
+
+        
+         public void Captura()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                string nom;
+                Console.WriteLine("EScriba nombre de los Alumnos:");
+                nom=Console.ReadLine();
+                Console.WriteLine("NOmbre es:" + nom);
+            }
+        }
+
+        
+             public void Edades()
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    int edad;
+                    Console.WriteLine("EScriba la edad de los alumnos");
+                    edad = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Edad es:" + edad);
+                }
+            }
+        }
+  }
+
+ 
+
---
+++ Parcial2/Parcial2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcion;
+            Console.WriteLine("INgrese un numero del 1 al 4");
+            opcion = int.Parse(Console.ReadLine());
+
+            switch(opcion)
+            {
+             case 1:
+                    M1 obj = new M1();
+                    obj.Multi();
+                    Console.ReadKey();
+             break;
+             case 2:
+                    M1 obj2 = new M1();
+                    obj2.Method2();
+                    Console.ReadKey();
+             break;
+
+                case 3:
+                    M1 obj3 = new M1();
+                    obj3.Captura();
+                    Console.ReadKey();
+                break;
+                case 4:
+                    M1 obj4 = new M1();
+                    obj4.Edades();
+                    Console.ReadKey();
+                break;
+                default:
+                Console.WriteLine("INgrese un numero del 1 al 4 unicamente");
+                break;
+            }
+        }
+    }
+}
